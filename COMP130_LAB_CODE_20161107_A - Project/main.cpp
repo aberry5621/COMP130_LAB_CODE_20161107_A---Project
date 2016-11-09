2726fa94fe7f523e09f9646848702b80db1f1683
aberry5621 <aberry5621@gmail.com>
2016-11-09 11:20:52 -0800

    Initial Commit

---
+++ COMP130_LAB_CODE_20161107_A - Project/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  COMP130_LAB_CODE_20161107_A - Project
+//
+//  Created by ax on 11/9/16.
+//  Copyright © 2016 COMP130. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
